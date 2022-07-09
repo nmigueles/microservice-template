15d595d7493e5062652fec63a59d1c5a60e3ac53
nmigueles <nicolasmigueles99@gmail.com>
2022-07-09 17:27:10 -0300

    :sparkles: Add tests

--- src/app.ts
+++ src/app.ts
@@ -8,4 +8,8 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/api", (req, res) => {
+  res.send();
+});
+
 export default app;
