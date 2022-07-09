d5f713f009fa0d75da3dcfb4110c324406bc638b
nmigueles <nicolasmigueles99@gmail.com>
2022-07-09 17:15:56 -0300

    :sparkles: Init

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
